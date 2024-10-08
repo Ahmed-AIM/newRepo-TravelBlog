2e916545ab37134de012b7f589b23ed3acb24017
Ahmed Atef <ahatif@hotmail.com>
2024-10-08 09:08:32 +0300

    Update apiConfig.js
    added link to the API "https://nodejs-serverless-function-express-neon-kappa.vercel.app/"

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,19 @@
 // API URLs
-const API_BASE_URL = 'http://localhost:5000/api';
-const POSTS_API = 'http://localhost:5000/api/posts';
+const API_BASE_URL = 'https://nodejs-serverless-function-express-neon-kappa.vercel.app/';
+const POSTS_API = `${API_BASE_URL}/posts`;
 const USERS_API = `${API_BASE_URL}/users`;
 
 // Export these constants so they can be imported in other files
 export { API_BASE_URL, POSTS_API, USERS_API };
+
+
+
+
+
+// // API URLs
+// const API_BASE_URL = 'http://localhost:5000/api';
+// const POSTS_API = 'http://localhost:5000/api/posts';
+// const USERS_API = `${API_BASE_URL}/users`;
+
+// // Export these constants so they can be imported in other files
+// export { API_BASE_URL, POSTS_API, USERS_API };
